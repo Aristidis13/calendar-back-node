b7e89dce659fe26541cf911ddb58a2f709062c7e
Aris Barlos <aristidis.barlos@gmail.com>
2025-09-09 16:50:28 +0300

    Minor refactors and er diagram addition

--- controllers/getMonthAvailability.js
+++ controllers/getMonthAvailability.js
@@ -3,6 +3,23 @@ import dayjs from 'dayjs';
 const DATE_FORMAT = 'YYYY-MM-DD';
 
 /**
+ * Creates mock data for dates
+ * @param {Number} year - The year we want to have the reservation
+ * @param {Number} month - The month we want to close the reservation
+ * @param {Number} numOfDays - The number of days of the month we want to close the reservation
+ */
+const createMockDates = (year, month, numOfDays) => {
+  const days = [];
+  for (let i = 1; i <= numOfDays; i++) {
+    days.push({
+      date: dayjs(`${year}-${month}-${i.toPrecision(2)}`).format(DATE_FORMAT),
+      is_full: Math.random() > 0.5 || i === 10 || i === 17 || i === 24 || i === 31, //Mock
+    });
+  }
+  return days;
+};
+
+/**
  * Returns how many days the given month of the year has
  */
 const createAvailableDays = selectedDate => {
@@ -13,32 +30,36 @@ const createAvailableDays = selectedDate => {
 };
 
 /**
- * For a specified maonth and year it receives the days the user can select
- * If the day is Sunday or the day has no available reservations to select
- * then it seems disabled.
+ * For a specified month and year it receives the days the user can select.
+ * If the day is Sunday or the day has no available reservations to select the day has `is_full` as disabled.
  */
 const getMonthAvailability = req => {
-  const { barberId: barberIdAsString, shopId: shopIdAsString, selectedDate = '' } = req.query;
+  const {
+    barberId: barberIdAsString,
+    shopId: shopIdAsString,
+    /*
+     * Will be used by DB to extract for specific shop and barber the hours that are available
+     */
+    selectedDate = '',
+  } = req.query;
   const [year, month] = selectedDate.split('-') || [];
+
+  /**
+   * Will be used by DB to extract data for the specific barber
+   */
   const barberId = parseInt(barberIdAsString);
+  /**
+   * Will be used by DB to extract data for the specific shop
+   */
   const shopId = parseInt(shopIdAsString);
-  const days = [];
 
   /**
    * Gets the selected month and year and gets an array with every day
    */
   const numOfDays = createAvailableDays(selectedDate);
-  console.log(selectedDate + ': numOfDays ' + numOfDays);
-
-  for (let i = 1; i <= numOfDays; i++) {
-    days.push({
-      date: dayjs(`${year}-${month}-${i.toPrecision(2)}`).format(DATE_FORMAT),
-      is_full: Math.random() > 0.5 || i === 10 || i === 17 || i === 24 || i === 31, //Mock
-    });
-  }
 
   return {
-    days,
+    days: createMockDates(numOfDays),
   };
 };
 
--- models/getReservations.js
+++ models/getReservations.js
@@ -6,57 +6,57 @@ import { convertTimeToUIFormat } from '../utils.js';
 const reservationsForMonth = [
   {
     id: 1,
-    date: '2025-08-12',
-    time: '10:00:00',
+    reservation_date: '2025-08-12',
+    reservation_time: '10:00:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 2,
-    date: '2025-08-27',
-    time: '10:30:00',
+    reservation_date: '2025-08-27',
+    reservation_time: '10:30:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 3,
-    date: '2025-08-06',
-    time: '11:00:00',
+    reservation_date: '2025-08-06',
+    reservation_time: '11:00:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 4,
-    date: '2025-08-04',
-    time: '11:30:00',
+    reservation_date: '2025-08-04',
+    reservation_time: '11:30:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 5,
-    date: '2025-08-12',
-    time: '10:00:00',
+    reservation_date: '2025-08-12',
+    reservation_time: '10:00:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 6,
-    date: '2025-08-05',
-    time: '10:30:00',
+    reservation_date: '2025-08-05',
+    reservation_time: '10:30:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 7,
-    date: '2025-08-05',
-    time: '11:00:00',
+    reservation_date: '2025-08-05',
+    reservation_time: '11:00:00',
     shop_id: 1,
     barber_id: 1,
   },
   {
     id: 8,
-    date: '2025-08-05',
-    time: '11:30:00',
+    reservation_date: '2025-08-05',
+    reservation_time: '11:30:00',
     shop_id: 1,
     barber_id: 1,
   },
@@ -71,7 +71,7 @@ const reservationsForMonth = [
  */
 const retrieveDatesForDay = (selectedDay, barberId, shopId) =>
   reservationsForMonth
-    .filter(res => selectedDay === res.date && res.barber_id === barberId && res.shop_id === shopId)
-    .map(res => convertTimeToUIFormat(res.time));
+    .filter(res => selectedDay === res.reservation_date && res.barber_id === barberId && res.shop_id === shopId)
+    .map(res => convertTimeToUIFormat(res.reservation_time));
 
 export { retrieveDatesForDay };
